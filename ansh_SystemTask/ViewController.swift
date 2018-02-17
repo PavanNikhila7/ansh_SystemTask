fe14bd2799f29a8ec8e25f8c4b84f0dd3a3e84db
pavankumarG <gpks.pavan@gmail.com>
2018-02-17 18:22:08 +0530

    Initial Commit

---
+++ ansh_SystemTask/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ansh_SystemTask
+//
+//  Created by Pavankumar G on 17/02/18.
+//  Copyright © 2018 Pavankumar G. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
